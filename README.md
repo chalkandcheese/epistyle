f88df718c6a20dc904d3c694f79b32afae1ad4af
Luis Borjas Reyes <luisfborjas@gmail.com>
2020-12-13 22:17:13 -0500

    Following spectre customization instructions
    https://picturepan2.github.io/spectre/getting-started/custom.html#installation

---
+++ README.md
@@ -0,0 +1,15 @@
+## Freenatalchart Theme
+
+A derivative of [Spectre](https://github.com/picturepan2/spectre), with our own extra styles common to `free.natalch.art` and other sites.
+
+### Building
+
+```sh
+npm install
+npx gulp watch
+# ^or npx gulp build for a one-off build.
+```
+
+### Distribution
+
+We currently list it on [`jsdelivr`](https://www.jsdelivr.com/features#gh)
---
+++ gulpfile.js
@@ -0,0 +1,30 @@
+const gulp = require("gulp");
+const sass = require('gulp-sass');
+const cleancss = require('gulp-clean-css');
+const csscomb = require('gulp-csscomb');
+const rename = require('gulp-rename');
+const autoprefixer = require('gulp-autoprefixer');
+
+function build() {
+  return gulp
+    .src('./src/*.scss')
+    .pipe(sass({outputStyle: 'compact', precision: 10})
+      .on('error', sass.logError)
+    )
+    .pipe(autoprefixer())
+    .pipe(csscomb())
+    .pipe(gulp.dest('./dist'))
+    .pipe(cleancss())
+    .pipe(rename({
+      suffix: '.min'
+    }))
+    .pipe(gulp.dest('./dist'));
+}
+
+function watch() {
+  gulp.watch('./**/*.scss', build);
+}
+
+exports.watch = watch;
+exports.build = build;
+exports.default = build;
