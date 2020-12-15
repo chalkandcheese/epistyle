112502ecae1348761e0739a792da558a9b0d84af
Luis Borjas Reyes <luisfborjas@gmail.com>
2020-12-15 12:25:01 -0500

    Rename, and add browserSync for development

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
-## Freenatalchart Theme
+## Firmament
 
-A derivative of [Spectre](https://github.com/picturepan2/spectre), with our own extra styles common to `free.natalch.art` and other sites.
+A derivative of [Spectre](https://github.com/picturepan2/spectre), with our own extra styles common to `freenatalchart.xyz` and other sites.
 
 ### Building
 
@@ -13,3 +13,15 @@ npx gulp watch
 ### Distribution
 
 We currently list it on [`jsdelivr`](https://www.jsdelivr.com/features#gh)
+
+Latest should be available at:
+
+```
+https://cdn.jsdelivr.net/gh/natal-chart/firmament/dist/base.min.css
+```
+
+Or by commit/tag:
+
+```
+https://cdn.jsdelivr.net/gh/natal-chart/firmament@f88df718c6a20dc904d3c694f79b32afae1ad4af/dist/base.min.css
+```
--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,11 @@
 const gulp = require("gulp");
+const { series } = require("gulp");
 const sass = require('gulp-sass');
 const cleancss = require('gulp-clean-css');
 const csscomb = require('gulp-csscomb');
 const rename = require('gulp-rename');
 const autoprefixer = require('gulp-autoprefixer');
+const browserSync = require("browser-sync").create();
 
 function build() {
   return gulp
@@ -18,13 +20,28 @@ function build() {
     .pipe(rename({
       suffix: '.min'
     }))
-    .pipe(gulp.dest('./dist'));
+    .pipe(gulp.dest('./dist'))
+    .pipe(browserSync.stream());
 }
 
 function watch() {
   gulp.watch('./**/*.scss', build);
 }
 
+function serve() {
+  browserSync.init({
+    server: {
+      baseDir: "./",
+      //directory: true,
+      index: "./dev/index.html"
+    }
+  });
+
+  gulp.watch("./**/*.scss", build);
+  gulp.watch("./dev/*.html").on('change', browserSync.reload);
+}
+
 exports.watch = watch;
 exports.build = build;
 exports.default = build;
+exports.serve = series(build, serve);
